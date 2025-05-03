0a116f7d9980b1990dbcf3ca74a5fa43122cfa9c
uumair327 <mohdumair.a@somaiya.edu>
2025-05-03 21:25:18 +0530

    firebae

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,16 @@ export default defineConfig({
     sourcemap: false,
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          firebase: ['firebase'],
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+              return 'vendor';
+            }
+            if (id.includes('firebase')) {
+              return 'firebase';
+            }
+            return 'vendor';
+          }
         },
       },
     },
